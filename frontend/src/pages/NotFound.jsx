6ba7dfb268ed9a5ee13109a7e229ce23455073b2
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-01-26 12:29:23 +0100

    Not Found page added

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
+function NotFound () {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        setTimeout(() => {
+            navigate("/")
+        }, 2000)
+    }, [])
+    return (
+        <h1>Not Found</h1>
+    )
+}
+
+export default NotFound
