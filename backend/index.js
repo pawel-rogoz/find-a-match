79fef8d0c3be9a8a015dd0b832f3f80f89a045da
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-01-28 23:29:09 +0100

    Authentication routes added
    Authentication routes added (auth and dashboard). One endpoint deleted

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,35 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express")
+const cors = require("cors")
+const pool = require("./db")
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/persons', (request, response) => {
-  response.json([{'id': 6, 'name': 'Paul'}, {'id': 7, 'name': 'Julia'}])
+app.use("/auth", require("./routes/jwtAuth"))
+
+app.use("/dashboard", require("./routes/dashboard"))
+
+app.post("/api/users", (request, response) => {
+  const { name } = request.body
+
+  pool.query('INSERT INTO users (name) VALUES ($1) RETURNING *', [name], (error, results) => {
+    if (error) {
+      throw error
+    }
+    console.log
+    response.status(201).send(`User added with ID: ${results.rows[0].user_id}`)
+  })
+})
+
+app.get("/api/users", (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY user_id', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
 })
 
 const PORT = 3001
