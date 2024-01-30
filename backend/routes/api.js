4b2ad9cd1cfa49c5049bbf41fed96e8a7cb23129
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-01-30 21:20:14 +0100

    Changes in API

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -6,7 +6,7 @@ const authorization = require("../middleware/authorization")
 router.get("/matches/:id/users", (request, response) => {
     const match_id = Number(request.params.id)
 
-    pool.query("SELECT user_id FROM users_in_matches WHERE match_id = $1", [match_id], (error, results) => {
+    pool.query("SELECT users.user_id, users.user_name FROM users_in_matches INNER JOIN users ON users_in_matches.user_id = users.user_id WHERE users_in_matches.match_id = $1", [match_id], (error, results) => {
         if (error) {
             throw error
         }
@@ -41,8 +41,8 @@ router.get("/objects/:id", (request, response) => {
     const object_id = Number(request.params.id)
 
     pool.query('SELECT * FROM objects WHERE object_id = $1', [object_id], (error, results) => {
-        if (error) {
-          throw error
+        if ((error) || (results.rows.length === 0)) {
+            response.status(400).send('ERROR: There was an error trying to get data')
         }
         response.status(200).json(results.rows[0])
     })
@@ -53,7 +53,7 @@ router.get("/matches", (request, response) => {
 
     pool.query('SELECT * FROM matches', (error, results) => {
         if (error) {
-          throw error
+            response.status(400).send('ERROR: There was an error trying to get data')
         }
         response.status(200).json(results.rows)
     })
@@ -64,8 +64,8 @@ router.get("/matches/:id", (request, response) => {
     const match_id = Number(request.params.id)
 
     pool.query('SELECT * FROM matches WHERE match_id = $1', [match_id], (error, results) => {
-        if (error) {
-          throw error
+        if ((error) || (results.rows.length === 0)) {
+            response.status(400).send('ERROR: There was an error trying to get data')
         }
         response.status(200).json(results.rows[0])
     })
@@ -78,7 +78,7 @@ router.post("/matches", authorization, (request, response) => {
 
     pool.query('INSERT INTO matches (object_id, host_id, match_date) VALUES ($1, $2, $3) RETURNING *', [object_id, host_id, match_date], (error, results) => {
         if (error) {
-          throw error
+          response.status(400).send('ERROR: There was an error adding this match to database')
         }
         response.status(200).json(results.rows[0])
     })
@@ -92,7 +92,7 @@ router.post("/matches/:id/users", authorization, (request, response) => {
 
     pool.query('INSERT INTO users_in_matches (match_id, user_id) VALUES ($1, $2) RETURNING *', [match_id, user_id], (error, results) => {
         if (error) {
-          throw error
+          response.status(400).send('ERROR: Perhaps user is already taking part in this match')
         }
         response.status(200).json(results.rows[0])
     })
