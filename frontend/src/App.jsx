8825a3364027023c9fa1ce2f19f9aab06a5f09a2
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-02-02 15:22:00 +0100

    Logic and new Routes in App.jsx
    Now username is being set for every render. Also new Routes added for new pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,10 +10,12 @@ import Object from "./components/Object"
 import Login from "./components/auth/Login"
 import Register from "./components/auth/Register"
 import Dashboard from "./components/auth/Dashboard"
+import AddMatch from "./pages/AddMatch"
 
 function App() {
-
   const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [name, setName] = useState(null)
+  const [id, setId] = useState(null)
 
   const setAuth = (isAuth) => {
     setIsAuthenticated(isAuth)
@@ -26,8 +28,14 @@ function App() {
       headers: { token: localStorage.token }
     })
     .then(response => response.data)
-    .then(parseResponse => {
-      parseResponse === true ? setIsAuthenticated(true) : setIsAuthenticated(false)
+    .then(response => {
+      if (response.authenticated === true) {
+        setIsAuthenticated(true)
+        setName(response.name)
+        setId(response.id)
+      } else {
+        setIsAuthenticated(false)
+      }
     })
     .catch(error => console.error(error.message))
   }
@@ -38,14 +46,15 @@ function App() {
 
   return (
     <>
-      <nav>
+      {/* <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/matches">Matches</Link></li>
         </ul>
-      </nav>
+      </nav> */}
+      {/* {name} */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home userName={name} userId={id} />} />
         <Route path="/login" element={
           !isAuthenticated ?
             <Login setAuth={setAuth}/>
@@ -67,7 +76,8 @@ function App() {
             <Navigate to="/login" replace={true} />
         }
         />
-        <Route path="/matches/*" element={<MatchRoutes />} />
+        <Route path="/add-match" element={<AddMatch />} />
+        <Route path="/matches/*" element={<MatchRoutes userName={name} userId={id}/>} />
         <Route path="/users/:id" element={<User />} />
         <Route path="/objects/:id" element={<Object />} />
         <Route path="*" element={<NotFound />}/>
