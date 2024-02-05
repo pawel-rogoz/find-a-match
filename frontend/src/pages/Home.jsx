bb03b8a436ac2b4e79ee913b0c5a80c4260de2cd
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-02-05 14:01:14 +0100

    Logic in Home changed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
-function Home ({ userName, userId }) {
+function Home ({ userData }) {
     const min_date = new Date()
     const max_date = new Date()
     max_date.setHours(23,59,59,999)
 
-    const [matches,setMatches] = useState([])
+    const [matches, setMatches] = useState([])
 
-    const handleClick = () => {
+    const { name, id } = userData
+
+    useEffect(() => {
+        console.log('effect')
         axios({
             method: 'get',
             url: 'http://localhost:3001/api/matchesBetweenDates',
@@ -17,19 +21,33 @@ function Home ({ userName, userId }) {
         .then(response => response.data)
         .then(response => setMatches(response))
         .catch(error => console.error(error))
+    }, [])
+
+    const formatDate = (date) => {
+        const options = { hour: '2-digit', minute: '2-digit', hour12: false}
+        const formattedDate = new Date(date).toLocaleString('en-US', options);
+        return formattedDate;
     }
 
     return (
         <>
             <div>
                 <h1>
-                    Hi{userName ? `, ${userName}` : ''}
+                    Hi{name ? `, ${name}` : ''}
                 </h1>
             </div>
             <div>
-                <h2>Matches</h2>
-                <button onClick={handleClick}>Click me</button>
-                {matches.map(match => <li key={match.match_id}>{match.match_name} {match.match_date}</li>)}
+                <h2>Today's Matches</h2>
+                {matches.length > 0 ? 
+                (
+                    <ul>
+                        {matches.map(match => <li key={match.match_id}><Link to={`/matches/${match.match_id}`}>{match.match_name}, godzina: {formatDate(match.match_date)}</Link></li>)}
+                    </ul>
+                )
+                :
+                (
+                    <p>There are no matches today</p>
+                )}
             </div>
         </>
     )
