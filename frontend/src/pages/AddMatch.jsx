d92b915df58a475e4499951104aa044334dc7a9d
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-02-05 13:06:27 +0100

    Picking object added to AddMatch

--- frontend/src/pages/AddMatch.jsx
+++ frontend/src/pages/AddMatch.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 
 function AddMatch() {
@@ -6,15 +6,26 @@ function AddMatch() {
         match_name: null,
         match_description: null,
         match_date: null,
-        num_players: null
+        num_players: null,
+        object_id: null
     })
+    const [objects, setObjects] = useState(null)
 
-    const { match_name, match_description, match_date, num_players } = data
-    const object_id = 1
+    const { match_name, match_description, match_date, num_players, object_id } = data
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/api/objects')
+            .then(response => response.data)
+            .then(response => setObjects(response))
+            .catch(error => console.error('Error', error))
+    }, [])
 
     const handleChange = (event) => {
-        console.log(event.target.id)
-        setData({...data, [event.target.id]: event.target.value})
+        const param_name = event.target.id
+        const param_value = param_name !== 'match_date' ? event.target.value : new Date(event.target.value).toISOString()
+
+        setData({...data, [param_name]: param_value})
     }
 
     const handleSubmit = (event) => {
@@ -42,14 +53,25 @@ function AddMatch() {
             }}
             onSubmit={handleSubmit}
             >
-                <label for="match_name">Match Name</label>
+                <label htmlFor="match_name">Match Name</label>
                 <input type="text" id="match_name" onChange={handleChange}/>
-                <label for="match_description">Match Description</label>
+                <label htmlFor="match_description">Match Description</label>
                 <input type="text" id="match_description" onChange={handleChange}/>
-                <label for="match_date" >Date and Time</label>
+                <label htmlFor="match_date" >Date and Time</label>
                 <input type="datetime-local" id="match_date" step={900} onChange={handleChange}/>
-                <label for="num_players">How many players?</label>
+                <label htmlFor="num_players">How many players?</label>
                 <input type="number" id="num_players" min="1" max="22" onChange={handleChange}/>
+                <label htmlFor="object_id">Choose an object:</label>
+                <select id="object_id" onChange={handleChange}>
+                    <option selected disabled value=''>Please choose an object</option>
+                    {objects ? (
+                        objects.map((object) => {
+                            return <option key={object.object_id} value={object.object_id}>{object.object_name}</option>
+                        })
+                    ) : (
+                        null
+                    )}
+                </select>
                 <input type="submit" value="submit" />
             </form>
         </div>
