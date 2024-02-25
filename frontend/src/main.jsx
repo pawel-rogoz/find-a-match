b12b55972953d2648fcd7d68338a75fff730287c
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-02-25 17:16:39 +0100

    Chakra UI added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+
+const styles = {
+  global:(props) => ({
+    body:{
+      bg:mode("gray.100", "#000")(props),
+      color:mode("gray.900","whiteAlpha.900")(props)
+    }
+  })
+}
+
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false
+}
+
+const theme = extendTheme({ config, styles})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>,
 )
