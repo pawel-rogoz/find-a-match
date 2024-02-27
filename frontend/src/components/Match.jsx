2d2cee12d3534276f7685834311e5296e20f8657
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-02-27 17:13:05 +0100

    Styling for Match changed

--- frontend/src/components/Match.jsx
+++ frontend/src/components/Match.jsx
@@ -3,15 +3,15 @@ import { IoIosTime } from "react-icons/io";
 import { FaUserFriends } from "react-icons/fa";
 import { Link as ReactRouterLink} from "react-router-dom"
 
-function Match({ match }) {
+function Match({ match, width }) {
     const date = new Date(match.match_date)
 
     const options = { hour: '2-digit', minute: '2-digit' }
     const locale = date.toLocaleTimeString('pl-PL', options)
 
     return (
-        <ChakraLink as={ReactRouterLink} to={`matches/${match.match_id}`}>
-            <Card w="25vw" maxW={300} mx="5vw" h="30vh" minH={150} maxH={200}>
+        <ChakraLink as={ReactRouterLink} to={`/matches/${match.match_id}`}>
+            <Card w={width} maxW={300} m={5} h="30vh" minH={150} maxH={200}>
                 <CardHeader h="30%">
                     <Flex justifyContent={['flex-start', 'flex-start', 'center']}>
                         <Heading size='l' noOfLines={1}>{match.match_name}</Heading>
