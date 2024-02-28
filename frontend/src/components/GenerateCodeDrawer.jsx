7c3fa03b16a2789a292e9548286b5474a158ecba
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-02-28 17:19:05 +0100

    GenerateCodeDrawer can now delete match from database

--- frontend/src/components/GenerateCodeDrawer.jsx
+++ frontend/src/components/GenerateCodeDrawer.jsx
@@ -1,9 +1,15 @@
-import { Drawer, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, DrawerBody, Input, Stack, Text, Button, HStack, GridItem, Grid, ButtonGroup, Box, Flex, Toast, useToast } from "@chakra-ui/react"
+import { Drawer, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, DrawerBody, Input, Stack, Text, Button, HStack, GridItem, Grid, ButtonGroup, Box, Flex, Toast, useToast, useDisclosure, AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay } from "@chakra-ui/react"
 import axios from "axios"
-import { useState } from "react"
+import { useRef, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 function GenerateCodeDrawer({ isOpen, onClose, btnRef, matchId}) {
     const toast = useToast()
+
+    const navigate = useNavigate()
+
+    const { isOpen: isDeleteOpen, onOpen: onDeleteOpen, onClose: onDeleteClose } = useDisclosure()
+    const cancelRef = useRef()
 
     const [display, setDisplay] = useState('none')
     const [isChosen, setIsChosen] = useState(false)
@@ -19,11 +25,29 @@ function GenerateCodeDrawer({ isOpen, onClose, btnRef, matchId}) {
         if ( id === 'yes' ) {
             setDisplay('flex')
         } else if ( id === 'no' ) {
+            onDeleteOpen()
             setDisplay('none')
         } else if ( id === 'generate') {
             generateMatchCode()
         }
         
+    }
+
+    const handleDelete = () => {
+        axios({
+            method: "DELETE",
+            url: `http://localhost:3001/api/matches/${matchId}`,
+            headers: {token: localStorage.token}
+        })
+        .then(response => {
+            onDeleteClose()
+            navigate("/")
+            toast({status: "success", title: "Match deleted", description: "Match is now deleted from DB", duration: 2000})
+        })
+        .catch(error => {
+            console.error(error)
+            toast({status: "error", title: "Something went wrong", description: "We didn't manage to delete match from DB. Try again", duration: 2000})
+        })
     }
 
     const generateMatchCode = () => {
@@ -67,7 +91,7 @@ function GenerateCodeDrawer({ isOpen, onClose, btnRef, matchId}) {
                             <Button id='yes' onClick={handleClick} colorScheme={isChosen ? 'gray' : 'green'}>YES</Button>
                             <Button id='no' onClick={handleClick} colorScheme={isChosen ? 'gray' : 'red'}>NO</Button>
                         </ButtonGroup>
-                        <Flex h='2rem'>
+                        <Flex display={display} h='2rem'>
                             <Text fontSize='2xl' as='b'>{code}</Text>
                         </Flex>
                         <Box display={display}>
@@ -77,6 +101,36 @@ function GenerateCodeDrawer({ isOpen, onClose, btnRef, matchId}) {
                     </DrawerBody>
                 </DrawerContent>
             </Drawer>
+
+            <AlertDialog
+                isOpen={isDeleteOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onDeleteClose}
+            >
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                            Delete Match
+                        </AlertDialogHeader>
+  
+                        <AlertDialogBody>
+                            Are you sure You want to delete this match? You can't undo this action afterwards.
+                        </AlertDialogBody>
+  
+                        <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={() => {
+                                onDeleteClose()
+                                setIsChosen(false)
+                            }}>
+                                Cancel
+                            </Button>
+                            <Button colorScheme='red' onClick={handleDelete} ml={3}>
+                                Delete
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
         </>
     )
 }
