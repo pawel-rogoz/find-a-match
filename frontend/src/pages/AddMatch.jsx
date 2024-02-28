4af5cc4ae537227013ad1a746f250f47ebd7f2b3
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-02-28 16:48:07 +0100

    AddMatch num_players change

--- frontend/src/pages/AddMatch.jsx
+++ frontend/src/pages/AddMatch.jsx
@@ -6,12 +6,13 @@ import { Button, Heading, Input, NumberDecrementStepper, NumberIncrementStepper,
 function AddMatch() {
     const navigate = useNavigate()
     const toast = useToast()
+    const defaultNumValue = 12
 
     const [data, setData] = useState({
         match_name: null,
         match_description: null,
         match_date: null,
-        num_players: null,
+        num_players: defaultNumValue,
         object_id: null
     })
     const [objects, setObjects] = useState(null)
@@ -61,38 +62,6 @@ function AddMatch() {
         })
     }
 
-    // return (
-    //     <div>
-    //         <form style={{
-    //             display: 'flex',
-    //             flexDirection: 'column'
-    //         }}
-    //         onSubmit={handleSubmit}
-    //         >
-    //             <label htmlFor="match_name">Match Name</label>
-    //             <input type="text" id="match_name" onChange={handleChange}/>
-    //             <label htmlFor="match_description">Match Description</label>
-    //             <input type="text" id="match_description" onChange={handleChange}/>
-    //             <label htmlFor="match_date" >Date and Time</label>
-    //             <input type="datetime-local" id="match_date" step={900} onChange={handleChange}/>
-    //             <label htmlFor="num_players">How many players?</label>
-    //             <input type="number" id="num_players" min="1" max="22" onChange={handleChange}/>
-    //             <label htmlFor="object_id">Choose an object:</label>
-    //             <select id="object_id" onChange={handleChange}>
-    //                 <option selected disabled value=''>Please choose an object</option>
-    //                 {objects ? (
-    //                     objects.map((object) => {
-    //                         return <option key={object.object_id} value={object.object_id}>{object.object_name}</option>
-    //                     })
-    //                 ) : (
-    //                     null
-    //                 )}
-    //             </select>
-    //             <input type="submit" value="submit" />
-    //         </form>
-    //     </div>
-    // )
-
     return (
         <Stack spacing={3} m={5} maxW={600}>
             <Heading>Want to add new match?</Heading>
@@ -104,7 +73,7 @@ function AddMatch() {
             <Text m={0}>Match's date</Text>
             <Input id="match_date" type='datetime-local' min={new Date} onChange={handleChange}></Input>
             <Text m={0}>Number of players</Text>
-            <NumberInput id="num_players" defaultValue={12} min={1} max={30} onChange={handleNumberChange}>
+            <NumberInput id="num_players" defaultValue={defaultNumValue} min={1} max={30} onChange={handleNumberChange}>
                 <NumberInputField id="num_players"/>
                 <NumberInputStepper>
                     <NumberIncrementStepper />
