b6dd47ce5d9c65ed7e962362509d9de849ddf701
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-02-29 18:33:49 +0100

    Login page changed
    Now it handles login and register actions and has different UI

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,75 +1,144 @@
 import { useState } from "react"
 import axios from "axios"
-import { Link } from "react-router-dom"
+import { Box, Button, Flex, Heading, Input, Stack, Text, Link, useToast } from "@chakra-ui/react"
 
 function Login ({ setUserData }) {
     const [data, setData] = useState({
         email: '',
-        password: ''
+        password: '',
+        name: ''
     })
-    
-    const [isUnsuccesfull, setIsUnsuccesfull] = useState(null)
 
-    const { email, password } = data
+    const toast = useToast()
+    const [isLogin, setIsLogin] = useState(true)
+    
+    const { email, password, name } = data
 
     const handleChange = (event) => {
+        console.log(event.target.value)
         setData({...data, [event.target.name]: event.target.value})
     }
 
+
     const handleSubmit = (event) => {
         event.preventDefault()
+        
+        let url, axiosData
+
+        if (isLogin) {
+            url = 'http://localhost:3001/auth/login'
+            axiosData = { email, password }
+        } else {
+            url = 'http://localhost:3001/auth/register'
+            axiosData = { email, password, name}
+        }
 
         axios({
             method: 'post',
-            url: 'http://localhost:3001/auth/login',
-            data: { email, password }
+            // url: 'http://localhost:3001/auth/login',
+            url: url,
+            data: axiosData
+            // data: { email, password }
         })
         .then(response => response.data)
         .then(response => {
             if (response.token) {
                 localStorage.setItem("token", response.token)
                 console.log(setUserData)
-                setUserData({name: response.name, id: response.id})
+                toast({ title: 'Success', description: 'You\'ll be redirected to dashboard', duration: 2000, status: 'success'})
+                setTimeout(() => setUserData({name: response.name, id: response.id}), 2000)
             } else {
-                // setAuth(false)
-                setIsUnsuccesfull(true)
+                toast({title: 'Error', description: 'Try again', status: 'error', isClosable: true, duration: 2000})
             }
         })
         .catch(error => {
             console.error(error)
-            setIsUnsuccesfull(true)
+            toast({title: 'Wrong credentials', description: 'Try again', status: 'error', isClosable: true, duration: 2000})
         })
     }
 
     return (
         <>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="email"
-                    className="form-control my-3"
-                    value={data.email}
-                    onChange={handleChange}
-                />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="password"
-                    className="form-control my-3"
-                    value={data.password}
-                    onChange={handleChange}
-                />
-                <button className="btn btn-success btn-block">Submit</button>
-            </form>
-            <Link to="/register">Don't have an account? Register</Link>
-            {isUnsuccesfull ? (
-                <p>Wrong credentials, try again</p>
-            ) : (
-                null
-            )
-            }
+            <Box mx={5}>
+                <Stack spacing={3}>
+                    <Heading mx='auto'>{isLogin ? 'Login' : 'Register'}</Heading>
+                    <form onSubmit={handleSubmit}>
+                        <Flex justifyContent='center'>
+                            <Input
+                                isRequired={true}
+                                type="email"
+                                name="email"
+                                placeholder="email"
+                                value={data.email}
+                                onChange={handleChange}
+                                bgColor='white'
+                                maxW={500}
+                            />
+                        </Flex>
+                        <Flex justifyContent='center'>
+                            <Input 
+                                isRequired={true}
+                                type="password"
+                                name="password"
+                                placeholder="password"
+                                value={data.password}
+                                onChange={handleChange}
+                                bgColor='white'
+                                mt={3}
+                                maxW={500}
+                            />
+                        </Flex>
+                        {
+                            isLogin ?
+                            (
+                                null
+                            )
+                            :
+                            (
+                                <Flex justifyContent='center'>
+                                    <Input
+                                        isRequired={true}
+                                        type="text"
+                                        name="name"
+                                        placeholder="name"
+                                        value={data.name}
+                                        onChange={handleChange}
+                                        bgColor='white'
+                                        mt={3}
+                                        maxW={500}                                
+                                    />
+                                </Flex>
+                            )
+                        }
+                        <Flex justifyContent='center'>
+                            <Button
+                                type="submit"
+                                colorScheme="teal"
+                                w='30vw'
+                                maxW={150}
+                                mt={5}
+                            >
+                                Submit
+                            </Button>
+                        </Flex>
+                    </form>
+                    {isLogin ?
+                        (
+                            <Text mx='auto' mt={3}>
+                                Don't have an account?{' '}
+                                <Link onClick={() => setIsLogin(false)} color='teal.400'>Register</Link>
+                            </Text>
+                        )
+                        :
+                        (
+                            <Text mx='auto' mt={3}>
+                                Do You have an account?{' '}
+                                <Link onClick={() => setIsLogin(true)} color='teal.400'>Login</Link>
+                            </Text>
+                        )
+                    }
+                </Stack>
+            </Box>
         </>
     )
 }
