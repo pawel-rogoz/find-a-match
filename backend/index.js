a74a2c0c69924cf3acef1b807e9184477a332119
pawel-rogoz <pawel.rogoz@poczta.onet.pl>
2024-02-29 23:03:47 +0100

    requestLogger middleware added

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,16 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
 app.use("/auth", require("./routes/jwtAuth"))
 
 app.use("/dashboard", require("./routes/dashboard"))
